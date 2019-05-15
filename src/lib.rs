mfkl/cc-rs

Add support for uwp targets

src/lib.rs
@@ -1219,7 +1219,8 @@ impl Build {
                     cmd.push_cc_arg("-ffunction-sections".into());
                     cmd.push_cc_arg("-fdata-sections".into());
                 }
-                if self.pic.unwrap_or(!target.contains("windows-gnu")) {
+                if self.pic.unwrap_or(!target.contains("windows-gnu") &&
+                                      !target.contains("uwp-gnu")) {
                     cmd.push_cc_arg("-fPIC".into());
                     // PLT only applies if code is compiled with PIC support,
                     // and only for ELF targets.
@@ -1781,6 +1782,8 @@ impl Build {
                         "armv7-unknown-netbsd-eabihf" => Some("armv7--netbsdelf-eabihf"),
                         "i586-unknown-linux-musl" => Some("musl"),
                         "i686-pc-windows-gnu" => Some("i686-w64-mingw32"),
+                        "i686-pc-uwp-gnu" => Some("i686-w64-mingw32"),
+                        "x86_64-pc-uwp-gnu" => Some("x86_64-w64-mingw32"),
                         "i686-unknown-linux-musl" => Some("musl"),
                         "i686-unknown-netbsd" => Some("i486--netbsdelf"),
                         "mips-unknown-linux-gnu" => Some("mips-linux-gnu"),

openharmony/third_party_rust_cc-rs

src/lib.rs
@@ -1783,6 +1783,7 @@ impl Build {
                         "armv7-unknown-netbsd-eabihf" => Some("armv7--netbsdelf-eabihf"),
                         "i586-unknown-linux-musl" => Some("musl"),
                         "i686-pc-windows-gnu" => Some("i686-w64-mingw32"),
+                        "i686-uwp-windows-gnu" => Some("i686-w64-mingw32"),
                         "i686-unknown-linux-musl" => Some("musl"),
                         "i686-unknown-netbsd" => Some("i486--netbsdelf"),
                         "mips-unknown-linux-gnu" => Some("mips-linux-gnu"),
@@ -1817,6 +1818,7 @@ impl Build {
                         "thumbv8m.main-none-eabi" => Some("arm-none-eabi"),
                         "thumbv8m.main-none-eabihf" => Some("arm-none-eabi"),
                         "x86_64-pc-windows-gnu" => Some("x86_64-w64-mingw32"),
+                        "x86_64-uwp-windows-gnu" => Some("x86_64-w64-mingw32"),
                         "x86_64-rumprun-netbsd" => Some("x86_64-rumprun-netbsd"),
                         "x86_64-unknown-linux-musl" => Some("musl"),
                         "x86_64-unknown-netbsd" => Some("x86_64--netbsd"),